6361ca9bdaf90b22d43a0166cf489ebd4f5d96df
Zayar Hla Khin <zayarhlakhin17@gmail.com>
2026-09-05 13:09:38 +0630

    Add boundary coverage for inclusive range sum

--- test_range_sum.py
+++ test_range_sum.py
@@ -3,7 +3,8 @@ from range_sum import inclusive_sum
 
 
 class TestInclusiveSum(unittest.TestCase):
-
+    def test_sum_of_zero(self):
+        self.assertEqual(inclusive_sum(0),0)
     def test_sum_includes_upper_bound(self):
         self.assertEqual(inclusive_sum(3), 6)
 
