b9fb80a67c3fa86b773729691a3f1bc41a4612e6
Zayar Hla Khin <zayarhlakhin17@gmail.com>
2026-09-05 15:17:40 +0630

    Add insufficient-stock regression coverage

--- test_inventory_rules.py
+++ test_inventory_rules.py
@@ -2,6 +2,8 @@ import unittest
 from inventory_rules import can_fulfill_order
 
 class TestInventoryRules(unittest.TestCase):
+    def test_order_above_stock_is_rejected(self):
+        self.assertFalse(can_fulfill_order(5,6))    
     def test_exact_stock_order_is_allowed(self):
         self.assertEqual(can_fulfill_order(5,5),True)
 
