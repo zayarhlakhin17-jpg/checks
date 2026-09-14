18998bcff3cf70c66d17297a03c3958a28e997da
Zayar Hla Khin <zayarhlakhin17@gmail.com>
2026-09-14 16:44:15 +0630

    Add inventory boundary tests

--- test_inventory_rules.py
+++ test_inventory_rules.py
@@ -8,5 +8,10 @@ class TestInventoryRules(unittest.TestCase):
     def test_exact_stock_order_is_allowed(self):
         self.assertEqual(can_fulfill_order(5,5),True)
 
+    def test_partial_stock_order_is_allowed(self):
+        self.assertTrue(can_fulfill_order(5,4))
+
+    def test_empty_stock_order_is_rejected(self):
+        self.assertFalse(can_fulfill_order(0,1))
 if __name__ == "__main__":
     unittest.main()
